fc58d94f0e182d565db16e741f66c92ac1cd44b8
Avinash <avinashunita@gmail.com>
2025-05-26 18:41:36 +0530

    fix

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,11 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host:  "containers-us-west-84.railway.app",
-  database:"railway",
-  password: "RIqVpHUHFpuNDVReUcexAMiywHaQQcAW",
+  host:  "localhost",
+  database:"your_database_name",
+  password: "your_password_here", // Replace with your actual password
   port:  5432
 });
 
 module.exports = pool;
+// deployed backend was not working, so using local backend
